db158a5d39e81bf4316e16467c1e46dfb09c8f8e
karthi <karthispirit@gmail.com>
2022-04-08 00:12:43 -0400

    Costco_test_code

--- src/main/java/ecommerce/Costco_main.java
+++ src/main/java/ecommerce/Costco_main.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 	import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.Test;
+
 	
 	public class Costco_main {
 
